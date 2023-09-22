b463d55407e917fbd4ce335d7fae5758c2d4e2ef
Siddhesh <siddheshvjadhav20@gmail.com>
2023-09-22 21:43:34 +0530

    1.morgan 2.ejs 3. rendered the index fiile

--- fullstackapp/app.js
+++ fullstackapp/app.js
@@ -1,21 +1,36 @@
 let express = require('express')
 let app = express();
+let fs = require('fs')
 let dotenv = require('dotenv')
+let morgan = require('morgan')
 dotenv.config()
 let port = process.env.PORT || 3113
-
 let categoryRouter = require('./src/controller/categoryRouter')
 let productRouter = require('./src/controller/productRouter')
 
-// Default url
+//logging info using fs system
+app.use(morgan('common', { stream: fs.createWriteStream('./app.log') }))
 
+// static files path
+app.use(express.static(__dirname + '/public'))
+
+// html path
+app.set('views', './src/views')
+
+// template engine 
+app.set('view engine', 'ejs')
+
+// Default url
 app.get('/', (req, res) => {
-    res.send("<h1>Hello welcome to full stack application</h1>")
+    // res.send("<h1>Hello welcome to full stack application</h1>")
+    res.render('index', { title: "Full stack app" })
 })
 
 
 app.use('/category', categoryRouter)
 app.use('/products', productRouter)
+
+
 app.listen(port, (err) => {
     if (err) throw err;
     else {
