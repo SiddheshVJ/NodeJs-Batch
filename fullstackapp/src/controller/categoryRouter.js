5308dbc6b762f91052ce645bd191dfff403c7e78
Siddhesh <siddheshvjadhav20@gmail.com>
2023-09-23 23:30:32 +0530

    datapushed in mongodb

--- fullstackapp/src/controller/categoryRouter.js
+++ fullstackapp/src/controller/categoryRouter.js
@@ -1,44 +1,35 @@
+let express = require('express');
+let categoryRouter = express.Router();
+let mongodb = require('mongodb').MongoClient;
+let url = process.env.MONGO_URL;
 
-let express = require('express')
-let categoryRouter = express.Router()
-
-
-const category = [
-    {
-        "id": 1,
-        "category": "Fashion",
-        "thumb": "https://i.ibb.co/56VP0Fn/cloths.jpg"
-    },
-    {
-        "id": 2,
-        "category": "Electronics",
-        "thumb": "https://i.ibb.co/pw5Wtdx/appliances.jpg"
-    },
-    {
-        "id": 3,
-        "category": "Essentials",
-        "thumb": "https://i.ibb.co/0cw34xm/essentials.jpg"
-    },
-    {
-        "id": 4,
-        "category": "Footwear",
-        "thumb": "https://i.ibb.co/r3SZq8S/footware.jpg"
-    }
-]
-
-function route(menu) {
-
+function router(menu) {
     categoryRouter.route('/')
         .get((req, res) => {
-            res.render('category', { title: 'Category Page', data: category, menu })
+            mongodb.connect(url, function (err, dc) {
+                if (err) {
+                    res.status(500).send('Error While connecting')
+                } else {
+                    let dbObj = dc.db('arpnode');
+                    dbObj.collection('category').find().toArray(function (err, category) {
+                        if (err) {
+                            res.status(203).send('Error While Fetching')
+                        } else {
+                            res.render('category', { title: 'Category Page', data: category, menu })
+                        }
+                    })
+                }
+            })
         })
 
     categoryRouter.route('/details')
         .get((req, res) => {
-            res.send("category details")
+            res.send('Category Details')
         })
 
     return categoryRouter
 }
 
-module.exports = route;
+
+
+module.exports = router
