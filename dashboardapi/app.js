56f452b2661d3a231bcf5bd775a20515366e54fe
Siddhesh <siddheshvjadhav20@gmail.com>
2023-10-02 21:04:06 +0530

    swagger

--- dashboardapi/app.js
+++ dashboardapi/app.js
@@ -4,22 +4,25 @@ import bodyParser from 'body-parser'
 import morgan from 'morgan'
 import dotenv from 'dotenv'
 import fs from 'fs'
-import { dbConnect } from './src/controller/dbController'
+
+import { ObjectId } from 'mongodb'
+import { dbConnect,addUser, getUser, updateUser, deleteUser } from './src/controller/dbController'
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json')
 dotenv.config()
 let app = express()
 let port = process.env.PORT || 3113
 
-import { addUser, getUser, updateUser, deleteUser } from './src/controller/dbController'
-import { ObjectId } from 'mongodb'
-
 // app log
 app.use(morgan('common', { stream: fs.createWriteStream('./app.log') }))
-
 
 // use middlewares
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
+
+app.use('/api-doc',swaggerUi.serve,swaggerUi.setup(swaggerDocument))
 
 //Health check
 app.get('/healthCheck', (req, res) => {
@@ -61,7 +64,7 @@ app.get('/users', async (req, res) => {
         query = { isActive }
     }
     else {
-        query = {}
+        query = { isActive: true }
     }
     const output = await getUser(query)
     res.status(200).send(output)
@@ -77,7 +80,7 @@ app.get('/user/:id', async (req, res) => {
 
 // update user
 app.put('/updateuser', async (req, res) => {
-    let condition = { _id: new ObjectId(req.query.id) }
+    let condition = { _id: new ObjectId(req.params.id) }
     let data = {
         $set: {
             "name": req.query.name,
@@ -100,6 +103,33 @@ app.delete('/deleteuser', async (req, res) => {
     res.send(response)
 })
 
+// soft delete :  deactivate user
+app.put('/deactivateuser', async (req, res) => {
+
+    let condition = { _id: new ObjectId(req.body._id) }
+    let data = {
+        $set: {
+            isActive: false
+        }
+    }
+
+    let response = await updateUser(condition, data)
+    res.status(200).send(response)
+})
+
+// activate user
+app.put('/activateuser', async (req, res) => {
+
+    let condition = { _id: new ObjectId(req.body._id) }
+    let data = {
+        $set: {
+            isActive: true
+        }
+    }
+
+    let response = await updateUser(condition, data)
+    res.status(200).send(response)
+})
 
 // running port
 app.listen(port, async (err) => {
